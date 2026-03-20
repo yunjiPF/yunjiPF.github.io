298e0e0e77e8d20856b086c51472f0bb4a7cf5bc
yunjiPF <kimyoon001017@gmail.com>
2026-03-20 14:11:39 +0900

    마지막

--- js/copyEmail.js
+++ js/copyEmail.js
@@ -1,4 +1,14 @@
 function copyEmail() {
-  navigator.clipboard.writeText('editor_sgy@gmail.com');
+  navigator.clipboard.writeText('kimyoon001017@gmail.com');
   alert('이메일 주소가 복사되었습니다!');
+}
+
+function copyNumber() {
+  navigator.clipboard.writeText('01071339858');
+  alert('전화번호가 복사되었습니다!');
+}
+
+function copykakao() {
+  navigator.clipboard.writeText('aaaaa1212');
+  alert('전화번호가 복사되었습니다!');
 }
